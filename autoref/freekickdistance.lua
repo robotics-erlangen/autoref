2048af5dcc1ec6c8e2251824e530aaa0d0216ddb
Michael Eischer <michael.eischer@robotics-erlangen.de>
2016-03-11 00:01:20 +0100

    Use ball position when free kick is called
    This ensures that the check still works even if the kicking robot pushes the ball

--- autoref/freekickdistance.lua
+++ autoref/freekickdistance.lua
@@ -2,14 +2,20 @@ local FreekickDistance = {}
 
 FreekickDistance.possibleRefStates = {
     Direct = true,
-    Indirect = true
+    Indirect = true,
+    Stop = true,
 }
 
 local offender
+local stopBallPos
 function FreekickDistance.occuring()
+    if World.RefereeState == "Stop" or not stopBallPos then
+        stopBallPos = World.Ball.pos
+        return
+    end
     local defense = World.RefereeState:match("irect(%a+)") == "Yellow" and "Blue" or "Yellow"
     for _, robot in ipairs(World[defense.."Robots"]) do
-        if robot.pos:distanceTo(World.Ball.pos)-robot.shootRadius < 0.5 then
+        if robot.pos:distanceTo(stopBallPos)-robot.shootRadius < 0.5 then
             offender = robot
             FreekickDistance.consequence = "STOP"
             return true
