67b1049ecafd5f3772b8c9784de02a20d0c8eed7
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2019-03-25 14:40:38 +0100

    Properly transform ball placement position to local space

--- base/world.lua
+++ base/world.lua
@@ -280,7 +280,9 @@ function World._updateGameState(state)
 	if state.designated_position and state.designated_position.x and
 			(not World.BallPlacementPos or World.BallPlacementPos.y ~= state.designated_position.y
 			or World.BallPlacementPos.x ~= state.designated_position.x) then
-		World.BallPlacementPos = Vector.createReadOnly(state.designated_position.x, state.designated_position.y)
+		World.BallPlacementPos = Vector.createReadOnly(
+			-state.designated_position.y / 1000,
+			state.designated_position.x / 1000)
 	end
 
 	World.GameStage = World.gameStageMapping[state.stage]
