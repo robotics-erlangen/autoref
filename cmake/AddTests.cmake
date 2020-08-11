cde797a8cbbebba8760e731bc4e7ba73eb6befea
Andreas Wendler <andreas.wendler@robotics-erlangen.de>
2020-08-11 19:51:39 +0200

    Add script to run new format autoref tests

--- cmake/AddTests.cmake
+++ cmake/AddTests.cmake
@@ -9,3 +9,15 @@ add_test(NAME luacheck-autoref
 # show what went wrong by default
 add_custom_target(check COMMAND ${CMAKE_CTEST_COMMAND} --output-on-failure
     USES_TERMINAL)
+
+# currently not copied (as the dependcies are not set) since these tests are not run right now
+add_custom_command(
+    OUTPUT ${CMAKE_BINARY_DIR}/run_autoref_tests.py
+    COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/cmake/run_autoref_tests.py ${CMAKE_BINARY_DIR}/run_autoref_tests.py
+    DEPENDS ${CMAKE_SOURCE_DIR}/cmake/run_autoref_tests.py
+)
+add_custom_command(
+    OUTPUT ${CMAKE_BINARY_DIR}/autoreftesthelper.lua
+    COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/cmake/autoreftesthelper.lua ${CMAKE_BINARY_DIR}/autoreftesthelper.lua
+    DEPENDS ${CMAKE_SOURCE_DIR}/cmake/autoreftesthelper.lua
+)
---
+++ cmake/run_autoref_tests.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+import sys
+import os
+import subprocess
+import re
+
+if len(sys.argv) != 3:
+	print("Usage: python3 run_autoref_tests.py <tests directory> <autoref location>")
+	exit(1)
+
+def createLuaScript(jsonFile):
+	def nameReplace(match):
+		name = match[0].replace(" ", "").replace("\t", "").replace(":", "")
+		# normalize to snake case
+		name = re.sub(r'(?<!^)(?=[A-Z])', '_', name).lower()
+		return "[" + name + "] ="
+
+	with open(jsonFile, "r") as f:
+		json = f.read()
+		filtered = re.sub('".*"\\ *\\t*:', nameReplace, json)
+		
+		with open("init.lua", "w") as initScript:
+			initScript.write('local Helper = require "autoreftesthelper"\n')
+			initScript.write('return Helper.testEvent(' + filtered + ')')
+
+exitCode = 0
+numFailingTests = 0
+for (dirPath, dirNames, fileNames) in os.walk(sys.argv[1]):
+	containsStrategy = False
+	for file in fileNames:
+		if file.endswith(".log"):
+			jsonFileName = file.replace(".log", ".json")
+			if not jsonFileName in fileNames:
+				print("No matching .json file found for log file " + file)
+				numFailingTests += 1
+				continue
+			fullName = os.path.join(dirPath, file)
+			fullJSONName = os.path.join(dirPath, jsonFileName)
+			createLuaScript(fullJSONName)
+			result = subprocess.run(["./bin/replay-cli", "-t", "init.lua", fullName, sys.argv[2]], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			if result.returncode != 0:
+				print(str(result.stdout, "utf8"))
+				print(str(result.stderr, "utf8"))
+				print("Test \"" + dirPath + "/" + file + "\" failed with exit code " + str(result.returncode))
+				numFailingTests += 1
+os.remove("init.lua")
+if numFailingTests == 0:
+	print("All tests successfull!")
+else:
+	print(str(numFailingTests) + " testcase(s) failed!")
+	exit(1)
